39b592b85ebb0e518aa602f85537cb4f3f11a7b9
NaathDess <nathalia.abade28@gmail.com>
2024-05-02 16:53:23 -0400

    exercício (concatenação operador soma com strings

--- concatenacao.js
+++ concatenacao.js
@@ -3,3 +3,4 @@ let nome = "Nathalia";
 let sobrenome = " Abade";
 let resultado = nome + "" + sobrenome;
 console.log(resultado);// saída = Nathalia Abade
+//da pra usar a concatenação com outros tipos de dados
