02ffb301eeb282a544ea04225ef586128a1aaf1f
NaathDess <nathalia.abade28@gmail.com>
2024-05-02 16:32:39 -0400

    exercício convert string para numero

---
+++ parseInt.js
@@ -0,0 +1,8 @@
+let stringParaNumero = "100";
+console.log(stringParaNumero);// saída;"100" -- string
+console.log(typeof(stringParaNumero));//saída -- identifica o tipo: string
+console.log("");
+//converção de string "stringParaNumber" para number
+let numero = parseInt(stringParaNumero);
+console.log(numero);// saíde: 100
+console.log(typeof(numero));// saída: -- tipo: numero
