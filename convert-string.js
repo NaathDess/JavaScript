1424171352473a23334a00f160560a5b792ee5d5
NaathDess <nathalia.abade28@gmail.com>
2024-05-02 16:23:31 -0400

    exercício convert numero para string

---
+++ convert-string.js
@@ -0,0 +1,5 @@
+let x = "A resposta é" + 70;
+let y = 70 + "é a resposta";
+console.log(x);//SAÍDA:"A resposta é 70"
+console.log(Y);//SAÍDA:"70 é a resposta"
+// o operador + converte o número para string
