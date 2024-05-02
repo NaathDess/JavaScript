5cb1ae3e6f621dd38f6f39d2f73a3f474976641e
NaathDess <nathalia.abade28@gmail.com>
2024-05-02 16:36:30 -0400

    exercício convert string para decimal

---
+++ parseFloat.js
@@ -0,0 +1,8 @@
+let stringParaDecimal = "12.60";
+console.log(stringParaDecimal);
+console.log(typeof(stringParaDecimal));
+console.log("")
+
+let decimal = parseFloat(stringParaDecimal);
+console.log(decimal);
+console.log(typeof(decimal));
