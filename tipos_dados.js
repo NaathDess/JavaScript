25bd915244726feee2e056670838ac1415ba70f2
NaathDess <nathalia.abade28@gmail.com>
2024-05-02 16:09:30 -0400

    exercício tipos de dados

---
+++ tipos_dados.js
@@ -0,0 +1,13 @@
+let nome = "Jaqueline";
+let verdadeiro = true;
+let falso = false;
+let numero = 26;
+let reais = 46.11;
+let nulo = null;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = {
+    name: "Jaqueline",
+    age: 26,
+    profission: "Bartender"
+}
