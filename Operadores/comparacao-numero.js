153076b3c8eec23b8cbbb52bd82ff7623f806375
NaathDess <nathalia.abade28@gmail.com>
2024-05-07 15:49:34 -0400

    pasta operadores

---
+++ Operadores/comparacao-numero.js
@@ -0,0 +1,12 @@
+let igualdade = 10 == 10;
+let diferente = 10 != 10;
+let menor     = 10  < 10;
+let menorQue  = 10 <= 10;
+let maior     = 10  > 10;
+let maiorQue  = 10 >= 10;
+console.log(`10 == 10: ${igualdade}`);  
+console.log(`10 != 10: ${diferente}`)
+console.log(`10 < 10: ${menor}`)
+console.log(`10 <= 10: ${menorQue}`)
+console.log(`10 > 10: ${maior}`)
+console.log(`10 >= 10: ${maiorQue}`)
---
+++ Operadores/comparacao-string.js
@@ -0,0 +1,8 @@
+let stringsIguais      = "Nathalia" == "Nathalia";
+let stringsDiferentes  = "nathalia" == "Nathalia";
+let stringMenor       = "A" < "B";
+let stringMaior       = "C" > "D";
+console.log(`Nathalia == Nathalia: ${stringsIguais}`);
+console.log(`nathalia == Nathalia: ${stringsDiferentes}`);
+console.log(`A < B: ${stringMenor}`);
+console.log(`C > D: ${stringMaior}`);
