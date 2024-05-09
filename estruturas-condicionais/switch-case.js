6b7094edadebc04663c1c056cedd08a559d54561
NaathDess <nathalia.abade28@gmail.com>
2024-05-09 14:49:14 -0400

    exemplo switch case

---
+++ estruturas-condicionais/switch-case.js
@@ -0,0 +1,23 @@
+function exibirInfoClasse(classe) {
+    switch (classe) {
+        case "Guerreiro":
+            console.log("Classe: Guerreiro");
+            console.log("Descrição: Os guerreiros são combatentes corpo a corpo resistentes, especializados no uso de armas e armaduras pesadas.");
+            console.log("Habilidade: Ataque poderoso, resistência a danos.");
+            break;
+            
+        case "Mago":
+            console.log("Classe: Mago");
+            console.log("Descrição: Os magos são mestres do poder arcano, capazes de lançar feitiços poderosos e controlar os elementos.");
+            console.log("Habilidade: Magias de ataque, controle elemental.");
+            break;
+
+        case "Ladino":
+            console.log("Classe: Ladino");
+            console.log("Descrição: Os ladrões são especialistas em furtividade e astúcia, capazes de realizar ataques furtivos e desarmar armadilhas.");
+            console.log("Habilidade: Furtividade, ataques precisos.");
+            break;
+            default:
+                console.log("Classe não reconhecida. Escolha entre Guerreiro, Mago ou Ladino");
+    }
+}
