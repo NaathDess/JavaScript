5507fa6ff9e0a2178bda367bef4fec9afd31995d
NaathDess <nathalia.abade28@gmail.com>
2024-05-14 13:52:35 -0400

    declaração função sem parâmetros mensagem

--- funcoes/declaracao-funcao-com-parametros.js
+++ funcoes/declaracao-funcao-com-parametros.js
@@ -1,6 +1,6 @@
 function nomeDaFuncao(parametro1, parametro2) {
 
     return resultado;
-}
+}// dentro das chaves é onde se coloca os códigos a serem executados com essa função
 
 nomeDaFuncao(parametro1, parametro2);
